c3d16efbd8794b251b0e4c62c8fd9a1220dfe557
{Gs48Yui} <>
2021-08-03 13:09:59 +0900

    nav変更（ハンバーガメニュー削除等）

--- resources/ts/components/organisms/layout/Footer.tsx
+++ resources/ts/components/organisms/layout/Footer.tsx
@@ -1,41 +1,54 @@
-import { Flex, Heading } from "@chakra-ui/react";
-import React from "react";
+import React, { useCallback } from "react";
 import { memo, VFC } from 'react';
+import { useHistory } from "react-router-dom";
+import { Box, Flex, Link } from "@chakra-ui/react";
+
+import { AddData } from "../../pages/AddData";
 
 
 export const Footer: VFC= memo(() => {
+  const history = useHistory();
+
+  const onClickMyData = useCallback(() => history.push("/home"), [history]);
+  const onClickShareData = useCallback(() => history.push("/home/sharedata"), [history]);
+  const onClickSettings = useCallback(() => history.push("/home/settings"), [history]);
+
   return(
-    <>
     <Flex
     minHeight="100vh"
     position="relative"
     paddingBottom="60px"
     boxSizing="border-box"
     >
-    <Flex
-    as="nav" 
-    bg="gray" 
-    color="gray.50" 
-    width="100%"
-    align="center" 
-    justify="space-between"
-    padding={{ base:3, md:5 }} 
-    position="fixed"
-    bottom= "0"
-    >
-      <Flex 
+      <Flex
+      as="nav" 
+      bg="gray" 
+      color="gray.50" 
+      width="100%"
+      align="center" 
+      justify="center"
+      padding={{ base:5, md:8 }} 
+      position="fixed"
+      bottom= "0"
+      >
+        <Flex 
         align="center" 
-        as="a" 
-        mr={8} 
+        fontSize="sm" 
         >
-        <Heading as="h1" fontSize={{ base:"md", md:"lg" }}>
-          DaTabi
-        </Heading>
+          <Box pr={10}>
+           <AddData />
+          </Box>
+          <Box pr={10}>
+            <Link onClick={onClickMyData}>私のデータ</Link>
+          </Box>
+          <Box pr={10}>
+            <Link onClick={onClickShareData}>みんなのデータ</Link>
+          </Box>
+            <Link onClick={onClickSettings}>設定</Link>
         </Flex>
     
+      </Flex>
     </Flex>
-    </Flex>
-    </>
   )
   
 });
--- resources/ts/components/organisms/layout/Header.tsx
+++ resources/ts/components/organisms/layout/Header.tsx
@@ -1,28 +1,19 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { memo,useCallback,VFC } from 'react';
-import { Box, Flex, Heading, Link, useDisclosure } from '@chakra-ui/react';
+import { Flex, Heading } from '@chakra-ui/react';
 import { useHistory } from 'react-router';
-
-import { MenuIconButton } from '../../atoms/button/MenuIconButton';
-import { MenuDrawer }  from '../../molecules/MenuDrawer';
-import { AddData } from "../../pages/AddData";
 
 
 export const Header: VFC= memo(() => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const history = useHistory();
 
   const onClickMyData = useCallback(() => history.push("/home"), [history]);
-  const onClickAddData = useCallback(() => history.push("/home/adddata"), [history]);
-  const onClickShareData = useCallback(() => history.push("/home/sharedata"), [history]);
-  const onClickSettings = useCallback(() => history.push("/home/settings"), [history]);
 
   return (
     <>
     <Flex 
       as="nav" 
-      bg="teal.300" 
+      bg="clear"
       color="gray.50" 
       align="center" 
       justify="space-between"
@@ -31,7 +22,8 @@ export const Header: VFC= memo(() => {
         <Flex 
         align="center" 
         as="a" 
-        mr={8} 
+        mr={8}
+        color="teal.500" 
         _hover={ {cursor:"pointer" }}
         onClick={onClickMyData}
         >
@@ -39,33 +31,7 @@ export const Header: VFC= memo(() => {
           DaTabi
         </Heading>
         </Flex>
-        <Flex 
-          align="center" 
-          fontSize="sm" 
-          flexGrow={2} 
-          display={{ base:"none", md:"flex" }} 
-        >
-          <Box pr={4}>
-           <AddData />
-          </Box>
-          <Box pr={4}>
-            <Link onClick={onClickMyData}>私のデータ</Link>
-          </Box>
-          <Box pr={4}>
-            <Link onClick={onClickShareData}>みんなのデータ</Link>
-          </Box>
-            <Link onClick={onClickSettings}>設定</Link>
-        </Flex>
-      <MenuIconButton onOpen={onOpen} /> 
     </Flex>
-    <MenuDrawer 
-    onClose={onClose} 
-    isOpen={isOpen} 
-    onClickMyData={onClickMyData} 
-    onClickAddData={onClickAddData} 
-    onClickShareData={onClickShareData} 
-    onClickSettings={onClickSettings} 
-    />
     </>
   )
 });
